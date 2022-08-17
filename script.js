ffe2f27542d7728d943300a5c9fe8876f4551f83
Josiane de Lima <108018406+josi-lima@users.noreply.github.com>
2022-08-17 09:46:01 -0300

    Add script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Scroll Reveal
+
+ScrollReveal().reveal(' #container02', { delay: 350 });
+ScrollReveal().reveal(' #container05', { delay: 350 });
+ScrollReveal().reveal(' #container07', { delay: 350 });
